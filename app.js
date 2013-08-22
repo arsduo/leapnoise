e6e07fcad2feb0d6988da3caa06024e028d3697a
Alex Koppel <alex@alexkoppel.com>
2013-08-21 23:26:54 -0500

    Add hold activation and integrate BlueGel changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Leap = require('leapjs');
 var BlueGel = require("bluegel");
 
-var filter = new BlueGel.Filter();
+var filter = new BlueGel(new Leap.Controller({enableGestures: true}));
 
 // import and set up our three filter types
 var filterTypes = [
@@ -10,11 +10,33 @@ var filterTypes = [
   require("./lib/track_filter.js")
 ];
 
+// control is by default deactivated to avoid accidental changes
+// to activate, hold your hand over the sensor for a second
+// you then have a second to make gestures and adjust
+// (activation timer resets after each gesture)
+var active = false, deactivateTimer, activate = function() {
+  active = true;
+  clearTimeout(deactivateTimer);
+  deactivateTimer = setTimeout(function() {
+    console.log("Control deactivated.");
+    active = false;
+  }, 2000);
+}
+
 var action;
 for (var i = 0; i < filterTypes.length; i++) {
   action = filterTypes[i];
-  filter.on(action.filterType, action.filterParameters, action.filter)
+  filter.on(action.filterType, action.filterParameters, function() {
+    // if (active) {
+      action.filter.apply(action, arguments);
+      // extend the activation time
+      activate();
+    // }
+  })
 }
 
-// need to bind the function properly to avoid needing the extra closure
-filter.drinkFromFirehose(new Leap.Controller({enableGestures: true}));
+filter.on("hold", {state: "start", minDuration: 500}, function(gesture) {
+  // the update event will be fired once the hold reaches the minimum duration
+  console.log("Activating control!");
+  activate();
+})
